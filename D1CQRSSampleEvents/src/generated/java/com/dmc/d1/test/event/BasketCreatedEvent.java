fdb4b30a124f6f94218bdbdcf0cb151f6d5f1e1c
davidclelland <dclelland@gmail.com>
2016-07-09 11:15:32 +0100

    command handler resolves aggregate automatically

--- D1CQRSSampleEvents/src/generated/java/com/dmc/d1/test/event/BasketCreatedEvent.java
+++
@@ -1,9 +0,0 @@
-package com.dmc.d1.test.event;
-
-import com.dmc.d1.cqrs.event.AggregateEvent;
-import com.dmc.d1.cqrs.util.StateEquals;
-import com.dmc.d1.test.domain.Basket;
-
-public interface BasketCreatedEvent extends AggregateEvent, StateEquals<BasketCreatedEvent> {
-  Basket getBasket();
-}
